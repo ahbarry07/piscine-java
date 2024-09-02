0d2b4956334b3be459402dc547dde346fa0711e3
ahbarry <ahmadou1.barry@gmail.com>
2024-09-02 14:41:03 +0000

    clean extraction 4th

--- CleanExtract/CleanExtract.java
+++ CleanExtract/CleanExtract.java
@@ -10,7 +10,7 @@ public class CleanExtract {
         }
 
         String goodWord;
-        goodWord = array[0];
+        goodWord = array[0].replace(".", "");
 
         for(int j = 1; j < array.length; j++){
             if(array[j].startsWith(".") && array[j].endsWith(".")){
@@ -22,7 +22,7 @@ public class CleanExtract {
     }
 
     public static void main(String[] args) {
-        System.out.println(extract("The|. quick brown. | what do you ..| .fox .|. Jumps over the lazy dog. ."));
+        System.out.println(extract(".The|. quick brown. | what do you ..| .fox .|. Jumps over the lazy dog. ."));
         System.out.println(extract("  | Who am .I  | .love coding,  |  |.  Coding is fun . | ...  "));
     }
 
