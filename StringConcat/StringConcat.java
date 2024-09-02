7a0aec77cd35fa9a0e83c66e7bef50ed65d81ca2
ahbarry <ahmadou1.barry@gmail.com>
2024-09-02 11:57:38 +0000

    string concat 2nd

--- StringConcat/StringConcat.java
+++ StringConcat/StringConcat.java
@@ -1,5 +1,10 @@
 public class StringConcat {
     public static String concat(String s1, String s2) {
+        if (s1 == null){
+            return s2;
+        }else if (s2 == null){
+            return s1;
+        }
         return s1 + s2;
     }
 
