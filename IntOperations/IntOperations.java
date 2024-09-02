4de3df7ecc4f67dbecbda776231f6716d11dea01
ahbarry <ahmadou1.barry@gmail.com>
2024-09-02 11:04:07 +0000

    comment main function

--- IntOperations/IntOperations.java
+++ IntOperations/IntOperations.java
@@ -12,22 +12,22 @@ public class IntOperations {
         return a / b;
     }
 
-    public static void main(String[] args) {
-        System.out.println("Add");
-        System.out.println(addTwoIntegers(1, 2));
-        System.out.println(addTwoIntegers(15, 68));
+    // public static void main(String[] args) {
+    //     System.out.println("Add");
+    //     System.out.println(addTwoIntegers(1, 2));
+    //     System.out.println(addTwoIntegers(15, 68));
 
-        System.out.println("Subtract");
-        System.out.println(subtractTwoIntegers(4, 1));
-        System.out.println(subtractTwoIntegers(10, 23));
+    //     System.out.println("Subtract");
+    //     System.out.println(subtractTwoIntegers(4, 1));
+    //     System.out.println(subtractTwoIntegers(10, 23));
 
-        System.out.println("Multiply");
-        System.out.println(multiplyTwoIntegers(3, 6));
-        System.out.println(multiplyTwoIntegers(12, 11));
+    //     System.out.println("Multiply");
+    //     System.out.println(multiplyTwoIntegers(3, 6));
+    //     System.out.println(multiplyTwoIntegers(12, 11));
 
-        System.out.println("Divide");
-        System.out.println(divideTwoIntegers(8, 2));
-        System.out.println(divideTwoIntegers(13, 4));
-    }
+    //     System.out.println("Divide");
+    //     System.out.println(divideTwoIntegers(8, 2));
+    //     System.out.println(divideTwoIntegers(13, 4));
+    // }
 
 }
