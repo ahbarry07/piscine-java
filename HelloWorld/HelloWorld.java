7205086cf79f54e804e1b3b873b745a466e4f0bc
ahbarry <ahmadou1.barry@gmail.com>
2024-09-02 10:48:51 +0000

    Hello world

---
+++ HelloWorld/HelloWorld.java
@@ -0,0 +1,13 @@
+package HelloWorld;
+
+public class HelloWorld {
+
+    public static String helloWorld(){
+        // System.out.println("Hello World !");
+        return "Hello World !";
+    }
+
+    public static void main(String[] args){
+        System.out.println(HelloWorld.helloWorld());
+    }
+}
