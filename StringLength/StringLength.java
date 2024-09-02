dc7899da76f7b47c45fb90248234929ad1d4b1f3
ahbarry <ahmadou1.barry@gmail.com>
2024-09-02 11:51:47 +0000

    string lenght 2nd

--- StringLength/StringLength.java
+++ StringLength/StringLength.java
@@ -1,5 +1,14 @@
 public class StringLength {
     public static int getStringLength(String s) {
+        if (s == null) {
+            return 0;
+        }
        return s.length();
     }
+
+    public static void main(String[] args) {
+        System.out.println(StringLength.getStringLength("Hello World !"));
+        System.out.println(StringLength.getStringLength(""));
+    }
+
 }
