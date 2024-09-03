4be02d0247d8566c081da2d7f96d106edea0c9d9
ahbarry <ahmadou1.barry@gmail.com>
2024-09-03 12:49:53 +0000

    cat

---
+++ Cat/Cat.java
@@ -0,0 +1,47 @@
+import java.io.*;
+
+public class Cat {
+    public static void cat(String[] args) throws IOException {
+        if (args.length == 0) {
+            return;
+        }
+
+        for (String filename : args) {
+            try (BufferedReader reader = new BufferedReader(new FileReader(filename))) {
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    System.out.println(line);
+                }
+            } catch (FileNotFoundException e) {
+                System.err.println("File not found: " + filename);
+            }
+        }
+
+    }
+
+
+    public static void main(String[] args) throws IOException {
+        PrintStream stdout = System.out;
+
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        PrintStream printStream = new PrintStream(outputStream);
+        System.setOut(printStream);
+
+        Cat.cat(new String[]{"input.txt"});
+        String output = outputStream.toString();
+        // Reset out to stdout
+        System.setOut(stdout);
+        System.out.println(output.equals("test input file\n"));
+
+        ByteArrayOutputStream outputStream2 = new ByteArrayOutputStream();
+        PrintStream printStream2 = new PrintStream(outputStream2);
+        System.setOut(printStream2);
+
+        Cat.cat(new String[]{});
+        String output2 = outputStream2.toString();
+        // Reset out to stdout
+        System.setOut(stdout);
+        System.out.println(output2.equals(""));
+    }
+
+}
