300722fd2b61d32060300c4401e70db8d5e14eb4
ahbarry <ahmadou1.barry@gmail.com>
2024-09-03 19:27:48 +0000

    search file

---
+++ FileSearch/FileSearch.java
@@ -0,0 +1,34 @@
+import java.io.*;
+// import java.nio.file.Files;
+
+public class FileSearch {
+    public static String searchFile(String fileName) {
+        File directory = new File("documents");
+        return findFile(fileName, directory);
+    }
+
+    private static String findFile(String name, File directory){
+        
+        File[] lists = directory.listFiles();
+
+        if (lists != null) {
+            for (File file : lists) {
+                if (file.isDirectory()) {
+                    
+                    String result = findFile(name, file);
+                    if (!result.equals("File not found")) {
+                        return result;
+                    }
+                } else if (name.equalsIgnoreCase(file.getName())) {
+                    return file.getPath();
+                }
+            }
+        }
+        return "File not found";
+    }
+
+    public static void main(String[] args) throws IOException {
+        System.out.println(searchFile("file.txt"));
+    }
+
+}
