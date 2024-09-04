2bfe5d01b564c28a04ac2bacdc723086f99d0718
ahbarry <ahmadou1.barry@gmail.com>
2024-09-04 11:23:17 +0000

    start constructor

--- StarProperties/CelestialObject.java
+++ StarProperties/CelestialObject.java
@@ -5,42 +5,19 @@ public class CelestialObject {
     public double z;
     public String name;
 
-    // public CelestialObject(){
-    //     this.x = 0.0;
-    //     this.y = 0.0;
-    //     this.z = 0.0;
-    //     this.name = null;
-    // }
-
     public CelestialObject(){
         this.x = 0.0;
         this.y = 0.0;
         this.z = 0.0;
-        this.name = "Soleil";
+        this.name = null;
     }
-
-    public CelestialObject(String name, double x, double y, double z){
-        this.x = x;
-        this.y = y;
-        this.z = z;
-        this.name = name;
-    }
-
-
 
     public static void main(String[] args) {
-        CelestialObject defaultStar = new CelestialObject();
-        System.out.println(defaultStar.x);
-        System.out.println(defaultStar.y);
-        System.out.println(defaultStar.z);
-        System.out.println(defaultStar.name);
-
-        CelestialObject earth = new CelestialObject("Terre", 0.43, 0.98, 1.43);
-        System.out.println(earth.x);
-        System.out.println(earth.y);
-        System.out.println(earth.z);
-        System.out.println(earth.name);
-
+        CelestialObject celestialObject = new CelestialObject();
+        System.out.println(celestialObject.x);
+        System.out.println(celestialObject.y);
+        System.out.println(celestialObject.z);
+        System.out.println(celestialObject.name);
     }
 
 }
