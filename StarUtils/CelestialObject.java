11c3b561244ca423b663f833b6801d16d81896c9
ahbarry <ahmadou1.barry@gmail.com>
2024-09-04 13:07:03 +0000

    last utils

--- StarUtils/CelestialObject.java
+++ StarUtils/CelestialObject.java
@@ -74,7 +74,7 @@ public class CelestialObject {
 
     public boolean equals(Object obj){
        if(this == obj) return true;
-       if(this.getClass() != obj.getClass() || obj == null) return false; 
+       if(obj == null || this.getClass() != obj.getClass()) return false; 
 
        CelestialObject other = (CelestialObject) obj;
 
