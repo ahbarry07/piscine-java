b570edddad23ec0ba6bb90ee4db95c81ba158b90
ahbarry <ahmadou1.barry@gmail.com>
2024-09-04 10:46:53 +0000

    celestial objet

---
+++ StarProperties/CelestialObject.java
@@ -0,0 +1,18 @@
+
+class CelestialObject {
+
+    public double x;
+    public double y;
+    public double z;
+    public String name;
+
+
+    public static void main(String[] args) {
+        CelestialObject celestialObject = new CelestialObject();
+        System.out.println(celestialObject.x);
+        System.out.println(celestialObject.y);
+        System.out.println(celestialObject.z);
+        System.out.println(celestialObject.name);
+    }
+
+}
