699798df23ab625b23757567079e9346a3a7bb21
ahbarry <ahmadou1.barry@gmail.com>
2024-09-04 11:34:16 +0000

    star guetters

---
+++ StarGetters/CelestialObject.java
@@ -0,0 +1,68 @@
+public class CelestialObject {
+
+    public double x;
+    public double y;
+    public double z;
+    public String name;
+
+    public CelestialObject(){
+        this.x = 0.0;
+        this.y = 0.0;
+        this.z = 0.0;
+        this.name = "Soleil";
+    }
+    
+    /******************** For X ****************/
+    public double getX(){
+        return this.x;
+    }
+
+    public void setX(double newX){
+        this.x = newX;
+    }
+
+    /******************** For Y ****************/
+    public double getY(){
+        return this.y;
+    }
+
+    public void setY(double newY){
+        this.y = newY;
+    }
+
+    /******************** For Z ****************/
+    public double getZ(){
+        return this.z;
+    }
+
+    public void setZ(double newZ){
+        this.z = newZ;
+    }
+
+    /********************For Name ****************/
+    public String getName(){
+        return this.name;
+    }
+
+    public void setName(String newName){
+        this.name = newName;
+    }
+    public static void main(String[] args) {
+        CelestialObject defaultStar = new CelestialObject();
+        System.out.println(defaultStar.getX());
+        System.out.println(defaultStar.getY());
+        System.out.println(defaultStar.getZ());
+        System.out.println(defaultStar.getName());
+
+        defaultStar.setName("Terre");
+        defaultStar.setX(0.43);
+        defaultStar.setY(0.98);
+        defaultStar.setZ(1.43);
+        System.out.println(defaultStar.getX());
+        System.out.println(defaultStar.getY());
+        System.out.println(defaultStar.getZ());
+        System.out.println(defaultStar.getName());
+
+    }
+
+}
