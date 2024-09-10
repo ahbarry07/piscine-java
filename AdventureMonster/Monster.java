d52979bf5ab05c67566c0904e85532981ab925b1
ahbarry <ahmadou1.barry@gmail.com>
2024-09-10 12:42:47 +0000

    Monster

---
+++ AdventureMonster/Monster.java
@@ -0,0 +1,26 @@
+
+public class Monster extends Character{
+
+    public Monster(String name, int maxHealth){
+        super(name, maxHealth);
+    }
+
+    @Override
+    public String toString(){
+        if (getCurrentHealth() > 0){
+            return String.format("%s s a monster with %s HP", getName(), getCurrentHealth());
+        }
+        return String.format("%s is a monster and is dead", getName());
+    }
+
+    public static void main(String[] args) {
+        Character aragorn = new Character("Aragorn", 20);
+        Monster slime = new Monster("Slime", 15);
+
+        System.out.println(Character.printStatus());
+
+        Character winner = Character.fight(aragorn, slime);
+
+        System.out.println(Character.printStatus());
+    }
+}
