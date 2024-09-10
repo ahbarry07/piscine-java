75606884d77791956382ff85b59243a3df5ee0cb
ahbarry <ahmadou1.barry@gmail.com>
2024-09-10 10:48:21 +0000

    private name

--- AdventureCharacter/Character.java
+++ AdventureCharacter/Character.java
@@ -2,7 +2,7 @@ public class Character {
     
     public final int maxHealth;
     public int currentHealth;
-    public final String name;
+    private final String name;
 
     public Character(String name, int maxHealth){
         this.name = name;
