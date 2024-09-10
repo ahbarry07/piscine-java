dcfaa55151bdb3426e3413b76483a30ad03ec656
ahbarry <ahmadou1.barry@gmail.com>
2024-09-10 13:25:07 +0000

    private health capacity

--- AdventureSorcerer/Sorcerer.java
+++ AdventureSorcerer/Sorcerer.java
@@ -1,7 +1,7 @@
 
 public class Sorcerer extends Character implements Healer{
 
-    public int healCapacity;
+    private int healCapacity;
 
     public Sorcerer(String name, int maxHealth, int healCapacity){
         super(name, maxHealth);
