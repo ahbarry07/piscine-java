c638d88935065b1660f0f683fc765af086e8332a
ahbarry <ahmadou1.barry@gmail.com>
2024-09-11 11:23:13 +0000

    last

--- ListEquals/ListEquals.java
+++ ListEquals/ListEquals.java
@@ -2,8 +2,8 @@ import java.util.List;
 
 public class ListEquals {
     public static boolean areListsEqual(List<String> list1, List<String> list2) {
-        if(list1.isEmpty() && list2.isEmpty()) return true;
         if (list1 != null){
+            if(list1.isEmpty() && list2.isEmpty()) return true;
             return list1.equals(list2);
         }
         return false;
