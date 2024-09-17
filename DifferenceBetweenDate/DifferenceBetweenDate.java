6b60c3b0c7374d8792d18aa6f0d7ede70cd13953
ahbarry <ahmadou1.barry@gmail.com>
2024-09-17 16:08:27 +0000

    diif date 2nd

--- DifferenceBetweenDate/DifferenceBetweenDate.java
+++ DifferenceBetweenDate/DifferenceBetweenDate.java
@@ -8,18 +8,21 @@ public class DifferenceBetweenDate {
 
     // Calcule la durée entre deux heures, toujours positive
     public static Duration durationBetweenTime(LocalTime localTime1, LocalTime localTime2) {
+        if (localTime1 == null || localTime2 == null) return null;
         // Utilise abs() pour s'assurer que la durée est toujours positive
         return Duration.between(localTime1, localTime2).abs();
     }
 
     // Calcule la période entre deux dates, toujours positive
     public static Period periodBetweenDate(LocalDate date1, LocalDate date2) {
+        if (date1 == null || date2 == null) return null;
         // Utilise abs() pour s'assurer que la période est toujours positive
         return Period.between(date1, date2).isNegative() ? Period.between(date2, date1) : Period.between(date1, date2);
     }
 
     // Calcule le nombre d'heures entre deux DateTimes, toujours positif
     public static Long numberOfHoursBetweenDateTime(LocalDateTime dateTime1, LocalDateTime dateTime2) {
+        if (dateTime1 == null || dateTime2 == null) return null;
         // Utilise abs() pour s'assurer que la durée est toujours positive
         return Math.abs(Duration.between(dateTime1, dateTime2).toHours());
     }
