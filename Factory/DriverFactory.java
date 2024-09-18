72002f88e879b61d633755e4df350f7de3653ab6
ahbarry <ahmadou1.barry@gmail.com>
2024-09-18 12:45:06 +0000

    factory 2nd

--- Factory/DriverFactory.java
+++ Factory/DriverFactory.java
@@ -1,12 +1,18 @@
 
 public class DriverFactory {
     
-    public Driver getDriver(String type){
+    public static Driver getDriver(String type){
         if (type.equalsIgnoreCase("Car")){
             return new CarDriver();
         }else if (type.equalsIgnoreCase("Plane")){
             return new PlaneDriver();
         }
         throw new IllegalArgumentException("Unknown driver type");
+    }
+
+    public static void main(String[] args)  {
+
+        System.out.println(DriverFactory.getDriver("Car").createTransport().getDistance());
+        System.out.println(DriverFactory.getDriver("Plane").createTransport().getDistance());
     }
 }
--- Factory/TransportFactory.java
+++ Factory/TransportFactory.java
@@ -9,4 +9,5 @@ public class TransportFactory {
         }
         return 0;
     }
+
 }
