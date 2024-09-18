05fcecc444474cba984e45cf5d1f90884fb5e697
ahbarry <ahmadou1.barry@gmail.com>
2024-09-18 14:52:43 +0000

    decorateur

--- Decorator/WithColdMeats.java
+++ Decorator/WithColdMeats.java
@@ -1,19 +1,17 @@
 
 public class WithColdMeats extends RacletteDecorator {
     
-    private Raclette decoratedRaclette;
-
     public WithColdMeats(Raclette raclette){
         super(raclette);
     }
 
     @Override
     public int getCalories(){
-        return this.decoratedRaclette.getCalories() + 350;
+        return this.getCalories() + 350;
     }
 
     @Override
     public String getIngredients(){
-        return this.decoratedRaclette.getIngredients() + ", charcuterie";
+        return this.getIngredients() + ", charcuterie";
     }
 }
--- Decorator/WithPickles.java
+++ Decorator/WithPickles.java
@@ -1,18 +1,16 @@
 
 public class WithPickles extends RacletteDecorator{
-    
-    private Raclette raclette;
 
     public WithPickles(Raclette raclette){
         super(raclette);
     }
     @Override
     public int getCalories(){
-        return this.raclette.getCalories() + 10;
+        return this.getCalories() + 10;
     }
 
     @Override
     public String getIngredients(){
-        return this.raclette.getIngredients() + ", cornichons";
+        return this.getIngredients() + ", cornichons";
     }
 }
